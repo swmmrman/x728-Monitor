a08ce4f65c4a91fba73629f4a90214a72aeaa154
Stephan Kern <stephan@swmmrman.com>
2021-04-07 05:36:16 -0600

    move x728pwr.sh to its own script and copy it

--- x728.sh
+++ x728.sh
@@ -9,42 +9,7 @@ sudo sed -i '$ i #Start power management on boot' /etc/rc.local
 
     sudo sed -e '/shutdown/ s/^#*/#/' -i /etc/rc.local
 
-    echo '#!/bin/bash
-
-SHUTDOWN=5
-REBOOTPULSEMINIMUM=200
-REBOOTPULSEMAXIMUM=600
-echo "$SHUTDOWN" > /sys/class/gpio/export
-echo "in" > /sys/class/gpio/gpio$SHUTDOWN/direction
-BOOT=12
-echo "$BOOT" > /sys/class/gpio/export
-echo "out" > /sys/class/gpio/gpio$BOOT/direction
-echo "1" > /sys/class/gpio/gpio$BOOT/value
-
-echo "X728 Shutting down..."
-
-while [ 1 ]; do
-  shutdownSignal=$(cat /sys/class/gpio/gpio$SHUTDOWN/value)
-  if [ $shutdownSignal = 0 ]; then
-    /bin/sleep 0.2
-  else
-    pulseStart=$(date +%s%N | cut -b1-13)
-    while [ $shutdownSignal = 1 ]; do
-      /bin/sleep 0.02
-      if [ $(($(date +%s%N | cut -b1-13)-$pulseStart)) -gt $REBOOTPULSEMAXIMUM ]; then
-        echo "X728 Shutting down", SHUTDOWN, ", halting Rpi ..."
-        sudo poweroff
-        exit
-      fi
-      shutdownSignal=$(cat /sys/class/gpio/gpio$SHUTDOWN/value)
-    done
-    if [ $(($(date +%s%N | cut -b1-13)-$pulseStart)) -gt $REBOOTPULSEMINIMUM ]; then
-      echo "X728 Rebooting", SHUTDOWN, ", recycling Rpi ..."
-      sudo reboot
-      exit
-    fi
-  fi
-done' > /etc/x728pwr.sh
+sudo cp x728pwr.sh /etc/x728pwr.sh
 sudo chmod +x /etc/x728pwr.sh
 sudo sed -i '$ i /etc/x728pwr.sh &' /etc/rc.local
 
