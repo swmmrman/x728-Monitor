61082be9e330000fc330da3cf5bf9dcaa7e40981
Stephan Kern <stephan@swmmrman.com>
2021-04-07 05:42:34 -0600

    Split out PLD script

--- x728.sh
+++ x728.sh
@@ -54,25 +54,5 @@ sudo chmod +x /home/pi/x728bat.py
 
     sudo sed -e '/button/ s/^#*/#/' -i /etc/rc.local
 
-    echo '#!/usr/bin/env python
-import RPi.GPIO as GPIO
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(6, GPIO.IN)
-
-def my_callback(channel):
-    if GPIO.input(6):     # if port 6 == 1
-        print "---AC Power Loss OR Power Adapter Failure---"
-    else:                  # if port 6 != 1
-        print "---AC Power OK,Power Adapter OK---"
-
-GPIO.add_event_detect(6, GPIO.BOTH, callback=my_callback)
-
-print "1.Make sure your power adapter is connected"
-print "2.Disconnect and connect the power adapter to test"
-print "3.When power adapter disconnected, you will see: AC Power Loss or Power Adapter Failure"
-print "4.When power adapter disconnected, you will see: AC Power OK, Power Adapter OK"
-
-raw_input("Testing Started")
-' > /home/pi/x728pld.py
+cp x728pld.py  /home/$(whoami)/x728pld.py
 sudo chmod +x /home/pi/x728pld.py
