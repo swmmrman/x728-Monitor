350756a87455868336898f944a114959d6d8e92b
Stephan Kern <stephan@swmmrman.com>
2021-04-09 06:22:42 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ if [ `whoami` != "root" ] || [ $UID == 0 ]; then
   echo "I must be run with sudo."
   exit 1;
 fi
-apt-get install python3-smbus i2c-tools 
+apt-get install python3-smbus i2c-tools python3-rpi.gpio
 echo -e 'i2c-dev' >> /etc/modules
 modprobe -a i2c-dev
 echo ds1307 0x68 > /sys/class/i2c-adapter/i2c-1/new_device
