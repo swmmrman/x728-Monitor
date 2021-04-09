0673d88cb15fed34a394208ca7c1d1c133d2eaea
Stephan Kern <stephan@swmmrman.com>
2021-04-09 06:42:23 -0600

    Add get_voltage

--- x728.py
+++ x728.py
@@ -18,6 +18,16 @@ def power_changed(channel):
         print(F"{current_time} Power Restored")
 
 
+def get_voltage(bus):
+    address = 0x36 # Address of the Battery gauge.
+    data_big_e = bus.read_word_data(address, 2)
+    #Convert from big to little endian
+    data_little_e= struct.unpack("<H", struct.pack(">H", data_big_e))[0]
+    #convert value to Voltage, numbers from manufacturer.
+    voltage = data_little_e * 1.25 / 1000 / 16
+    return voltage
+
+
 bus = smbus.SMBus(1) # setup the SMBus to read from.
 
 PINS = {
