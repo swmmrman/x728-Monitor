7f14fe49f296ea93ba760fb56677a50b07b34b48
Stephan Kern <stephan@swmmrman.com>
2021-10-14 12:39:51 -0600

    Remove = from compare

--- x728-monitor.py
+++ x728-monitor.py
@@ -79,7 +79,7 @@ def main():
     config.read(conf_file)
     version = float(config['DEVICE']['version'].strip(';'))
     DEBUG = True if config['PARAMETERS']['debug'].strip() == "true" else False
-    if version <= 2:
+    if version < 2:
         PINS['OFF'] = 13  # Change if older x728
     TIMEOUT = int(config['PARAMETERS']['timeout'])
     time_left = TIMEOUT
