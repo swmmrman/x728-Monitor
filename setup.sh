f7f48b4995204fd27eaca930a75f6ef49e950b44
Stephan Kern <stephan@swmmrman.com>
2022-03-30 20:07:27 -0600

    Add a small delay to allow the i2c interface to start

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ modprobe -a i2c-dev
 echo ds1307 0x68 > /sys/class/i2c-adapter/i2c-1/new_device
 sed -i '$ i echo ds1307 0x68 > /sys/class/i2c-adapter/i2c-1/new_device' /etc/rc.local
 sed -i '$ i hwclock -s' /etc/rc.local
+sleep 2
 hwclock -w
 
 cp x728.conf /etc/
