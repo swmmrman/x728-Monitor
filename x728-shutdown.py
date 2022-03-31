541294753a4f78a0aa575f6abb4d88527cadbad0
Stephan Kern <stephan@swmmrman.com>
2022-03-30 20:13:36 -0600

    Quick shutdown script

---
+++ x728-shutdown.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import smbus
+import time
+import os
+import sys
+import x728
+import RPi.GPIO as GPIO
+
+def main():
+    if(os.getuid() != 0):
+        print("This must be run as root")
+        sys.exit(1)
+    GPIO.setmode(GPIO.BCM)
+    for i in 6,12,26:
+        GPIO.setup(i, GPIO.OUT)
+    ups = x728.x728(2.1, smbus.SMBus(1))
+    ups.call_shutdown()
+    sys.exit(0)
+
+main()
